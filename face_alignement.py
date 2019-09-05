ae4e009105976bd16d0d2c7627194c0f73ae0ee6
HimikoV <lukasz.kos98@gmail.com>
2019-09-05 18:56:00 +0200

    nic

--- face_alignement.py
+++ face_alignement.py
@@ -1,19 +1,21 @@
 import face_alignment
 import cv2
 import time
-poczatek=time.time()
+
+poczatek = time.time()
 import numpy as np
-fa = face_alignment.FaceAlignment(face_alignment.LandmarksType._2D,flip_input=False,device='cuda')
+
+fa = face_alignment.FaceAlignment(face_alignment.LandmarksType._2D, flip_input=False, device='cpu')
 input = cv2.imread("testwojtek.png")
 preds = fa.get_landmarks(input)
 
-# lista=[]
+# lista = []
 # for i in range(len(preds[0])):
-#     lista.append((preds[0][i][0],preds[0][i][1]))
-# print(type(lista[0]))
-koniec=time.time()
-print(koniec-poczatek)
+#     lista.append((preds[0][i][0], preds[0][i][1], preds[0][i][2]))
+# print(lista)
+koniec = time.time()
+print(koniec - poczatek)
 for i in range(len(preds[0])):
-    cv2.circle(input,(preds[0][i][0],preds[0][i][1]),1,(0,0,0))
-cv2.imshow('kozak',input)
+    cv2.circle(input, (preds[0][i][0], preds[0][i][1]), 2, (0, 255, 0),-1)
+cv2.imshow('kozak', input)
 cv2.waitKey(0)
